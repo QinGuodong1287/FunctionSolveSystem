6c4bfad32a8ecb69bec61501aad6140babb86738
Qin Guodong <qinguodong07@qq.com>
2021-01-26 11:05:34 +0800

    Add files via upload

---
+++ FunctionSolveSystem/utils/spliter/FunctionSpliter.java
@@ -0,0 +1,92 @@
+package utils.spliter;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import utils.spliter.SplitResult;
+import utils.checker.FunctionChecker;
+import utils.exceptions.InvaildValueException;
+
+public class FunctionSpliter {
+	protected static double a = 0, b = 0, c = 0;
+	protected static int exponent = 0;
+	
+	private FunctionSpliter() {
+	}
+	
+	public static SplitResult split(String function) throws InvaildValueException {
+		// Remove all spaces, add a symbol to the home and end.
+		function = function.charAt(0) == '+' || function.charAt(0) == '-'? function: "+" + function;
+		// Check function string is vaild.
+		if(!FunctionChecker.check(function)) {
+			// If the function string is invaild, throw a InvaildValueException.
+			throw new InvaildValueException("This function string is invaild.");
+		}
+		function += "+";
+		splitCV(function);
+		if(a != 0) {
+			exponent = 2;
+		} else if(b != 0) {
+			exponent = 1;
+		} else {
+			exponent = 0;
+		}
+		return new SplitResult(a, b, c, exponent);
+	}
+	
+	protected static void splitCV(String function) {
+		// Get three parts.
+		String[] patterns = {
+			"(?:\\+|-)\\d*(?:\\.\\d*)?x\\^2(?:\\+|-)", 
+			"(?:\\+|-)\\d*(?:\\.\\d*)?x(?:\\+|-)", 
+			"(?:\\+|-)\\d+(?:\\.\\d*)?(?:\\+|-)", 
+		};
+		String[] zeros = {
+			"0x^2+", 
+			"0x+", 
+			"0+"
+		};
+		String[] parts = new String[3];
+		Matcher m;
+		for(int i = 0; i < 3; i ++) {
+			m = Pattern.compile(patterns[i]).matcher(function);
+			if(m.find()) {
+				parts[i] = m.group();
+			} else {
+				parts[i] = zeros[i];
+			}
+		}
+		// Get first CV.
+		parts[0] = parts[0].substring(0, parts[0].length() - 1);
+		if(parts[0].charAt(0) == 'x') {
+			parts[0] = "+1x^2";
+		} else if(parts[0].charAt(1) == 'x') {
+			if(parts[0].charAt(0) == '+') {
+				parts[0] = "+1x^2";
+			} else if(parts[0].charAt(0) == '-') {
+				parts[0] = "-1x^2";
+			}
+		}
+		parts[0] = parts[0].substring(0, parts[0].length() - 3);
+		a = Double.parseDouble(parts[0]);
+		// Get second CV.
+		parts[1] = parts[1].substring(0, parts[1].length() - 1);
+		if(parts[1].charAt(0) == 'x') {
+			parts[1] = "+1x";
+		} else if(parts[1].charAt(1) == 'x') {
+			if(parts[1].charAt(0) == '+') {
+				parts[1] = "+1x";
+			} else if(parts[1].charAt(0) == '-') {
+				parts[1] = "-1x";
+			}
+		}
+		parts[1] = parts[1].substring(0, parts[1].length() - 1);
+		b = Double.parseDouble(parts[1]);
+		// Get third CV.
+		parts[2] = parts[2].substring(0, parts[2].length() - 1);
+		if("".equals(parts[2])) {
+			parts[2] = "0";
+		}
+		c = Double.parseDouble(parts[2]);
+	}
+}
---
+++ FunctionSolveSystem/utils/spliter/SplitResult.java
@@ -0,0 +1,13 @@
+package utils.spliter;
+
+public class SplitResult {
+	public double cv1, cv2, cv3;
+	public int exponent;
+	
+	public SplitResult(double a, double b, double c, int exponent) {
+		this.cv1 = a;
+		this.cv2 = b;
+		this.cv3 = c;
+		this.exponent = exponent;
+	}
+}
