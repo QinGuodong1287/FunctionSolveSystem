31a71e92ac39280c5b57cba8c1c4df94f013ebe6
Qin Guodong <qinguodong07@qq.com>
2021-01-26 10:51:33 +0800

    Add files via upload

---
+++ FunctionSolveSystem/utils/checker/FunctionChecker.java
@@ -0,0 +1,28 @@
+package utils.checker;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class FunctionChecker {
+	protected static String[] patterns = {
+		"^$", 
+		"[\u4e00-\u9fa5]", 
+		"[^x0-9\\.\\^\\+\\-]", 
+		"x\\^2[^\\+\\-]", 
+		"x\\^[^2]", 
+		"x\\^$", 
+		"x[^\\^\\+\\-]", 
+		"\\d+[^\\.\\d+\\+\\-x]", 
+		"\\d+[\\+\\-]$", 
+		"^[\\+\\-]$"
+	};
+	
+	public static boolean check(String function) {
+		for(int i = 0; i < patterns.length; i ++) {
+			if(Pattern.compile(patterns[i]).matcher(function).find()) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
