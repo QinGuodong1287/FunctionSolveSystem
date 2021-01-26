3e9191986ecd9356fa10591a3156f6d0689bb5b8
Qin Guodong <qinguodong07@qq.com>
2021-01-26 19:30:21 +0800

    Add files via upload

---
+++ FunctionSolveSystem/utils/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package utils.config;
+
+public class ApplicationConfig {
+	private ApplicationConfig() {
+	}
+	
+	// These are the application's config.
+	// Enable or disable the chinese translator service.
+	public static boolean enableChineseTranslatorService = false;
+}
