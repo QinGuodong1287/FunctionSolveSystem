ae77afabe468aef1e47c815db207329549a2cd85
Qin Guodong <qinguodong07@qq.com>
2021-01-31 14:31:16 +0800

    Update ApplicationConfiger.bat

--- bin/ApplicationConfiger.bat
+++ bin/ApplicationConfiger.bat
@@ -6,6 +6,11 @@ rem This script can config this application.
 rem Initizlize.
 echo Initializing...
 
+rem Save old path.
+for /F %%command in ('chdir') do (
+	set "OldPath=%%command"
+)
+
 rem Find the config file.
 set "FileName=ApplicationConfig.java"
 for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
@@ -26,4 +31,6 @@ echo Editing...
 rem Use editor to edit config file(defualt editor is notepad)
 notepad %ConfigFilePath%
 echo Compiling...
+cd ..\..\..
 javac %ConfigFilePath%
+cd %OldPath%
