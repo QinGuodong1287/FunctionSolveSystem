4e0a62c5a16065cb0401771ce21dbf7ee86869d1
Qin Guodong <qinguodong07@qq.com>
2021-01-31 16:10:54 +0800

    Add files via upload

--- bin/ApplicationConfiger.bat
+++ bin/ApplicationConfiger.bat
@@ -1,36 +1,59 @@
 @echo off
 
+
 rem Note:
+
 rem This script can config this application.
 
-rem Initizlize.
+
+rem Initialize.
 echo Initializing...
 
+
 rem Save old path.
-for /F %%command in ('chdir') do (
-	set "OldPath=%%command"
+for /F %%i in ('chdir') do (
+	set "OldPath=%%i"
 )
 
+
 rem Find the config file.
+
 set "FileName=ApplicationConfig.java"
+
 for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+
 	if exist %%a:\ (
+
 		pushd %%a:\
 		for /r %%b in (*%FileName%) do (
 			if /i "%%~nxb" equ "%FileName%" (
-				set "ConfigFilePath=%%b"
+				for /f %%i in ('chdir') do (
+					set "Disk=%%i"
+				)
+				set "LogFilePath=%%~dpb"
+
 				break
+
 			)
 		)
 		popd
 	)
+
 )
+cd /d %Disk%
 
 rem Edit the config file.
 echo Editing...
+
 rem Use editor to edit config file(defualt editor is notepad)
-notepad %ConfigFilePath%
+
+notepad "%LogFilePath%\ApplicationConfig.java"
+
+
+rem Compile the config file.
 echo Compiling...
-cd ..\..\..
-javac %ConfigFilePath%
+cd %LogFilePath%
+cd ..\..\..\..
+javac "com\QinGuodong1287\FunctionSolveSystem\config\ApplicationConfig.java"
 cd %OldPath%
+pause
--- bin/FunctionSolveSystemLuncher.bat
+++ bin/FunctionSolveSystemLuncher.bat
@@ -1,29 +1,57 @@
 @echo off
 
+
 rem Note:
+
 rem This script can lunach this application.
 
+
+
 rem Save old path.
-for /F %%command in ('chdir') do (
-	set "OldPath=%%command"
+
+for /F %%i in ('chdir') do (
+
+	set "OldPath=%%i"
+
 )
+
+
 
 rem Find the application's main file.
+
 set "FileName=FunctionSolveSystem.class"
+
 for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+
 	if exist %%a:\ (
+
 		pushd %%a:\
+
 		for /r %%b in (*%FileName%) do (
+
 			if /i "%%~nxb" equ "%FileName%" (
-				set "MainFilePath=%%a/%%b"
+
+				set "MainFilePath="%%~dpb"
+
 				break
+
 			)
+
 		)
+
 		popd
+
 	)
+
 )
 
+
+
 rem Execute the main file.
-cd %MainFilePath%
-java FunctionSolveSystem
+
+cd "%MainFilePath%\..\..\.."
+java com.QinGuodong1287.FunctionSolveSystem.FunctionSolveSystem
+
 cd %OldPath%
+
+pause
